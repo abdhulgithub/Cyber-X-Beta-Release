0be67faab22082692f6d6855b621ad57ec37e02d
abdhulgithub <111677291+abdhulgithub@users.noreply.github.com>
2022-08-24 21:57:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,28 +36,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94763867158'] //ur owner number
+global.ownername = "❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱" //ur owner name
+global.ytname = "YT:GAMING KENZO" //ur yt chanel name
+global.socialm = "whatsapp:Follow this link to join my WhatsApp group: https://chat.whatsapp.com/H047x1gxkO9F6z3UhoiMef" //ur github or insta name
+global.location = "Sri Lanka, matale, ukuwela" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '9471421243' //ur tag number
-global.botname = 'CYBER-X TEST EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
+global.owner = ['94763867158']
+global.ownertag = '94763867158' //ur tag number
+global.botname = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱ TEST EDITION' //ur bot name
+global.ig = "KENZO FF" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
+global.reactmoji = "" //ur menu react emoji
 global.themeemoji = "🚀" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.aliveimg ="https://telegra.ph/file/71c859102f3625d06204b.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.packname = "❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱ Test Bot" //ur sticker watermark packname
+global.author = "❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱" //ur sticker watermark author
+global.aliveimg ="https://https://i.ibb.co/VTxVmF6/20220821-153441.jpg.jpg"
+global.allmenuimg ="https://https://i.ibb.co/VTxVmF6/20220821-153441.jpg .jpg"
+global.welcomeimg= "https://https://i.ibb.co/VTxVmF6/20220821-153441.jpg.jpg"
+global.gbyeimg = "https://https://i.ibb.co/VTxVmF6/20220821-153441.jpg .jpg"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -67,7 +67,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94763867158'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
